8c58f0bf5246df12dd02c44f758e26d623f4ffad
Wayne Chaw <wayne.chaw@gmail.com>
2024-04-24 19:12:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const app = express();
+const port = process.env.PORT || 4000;
+
 
 app.use(express.static('dist/my-app/browser'));
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
