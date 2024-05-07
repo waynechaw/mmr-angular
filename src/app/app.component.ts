66004382afa8f9760591376f67239a44b363331e
Wayne Chaw <wayne.chaw@gmail.com>
2024-05-06 21:09:40 -0700

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,6 @@ export class AppComponent implements OnInit {
       "https://www.iamag.co/wp-content/uploads/2023/06/kudos-productions-thousand-pierced-volibear.jpg",
       "https://www.iamag.co/wp-content/uploads/2023/06/kudos-productions-airis.jpg",
       "https://www.iamag.co/wp-content/uploads/2020/09/The-Art-of-Legends-of-Runeterra-52.jpg",
-      "https://www.iamag.co/wp-content/uploads/2020/09/The-Art-of-Legends-of-Runeterra-92.jpg",
       "https://www.iamag.co/wp-content/uploads/2020/09/The-Art-of-Legends-of-Runeterra-112.jpg",
       "https://www.iamag.co/wp-content/uploads/2020/09/The-Art-of-Legends-of-Runeterra-129.jpg",
       "https://www.iamag.co/wp-content/uploads/2020/09/The-Art-of-Legends-of-Runeterra-183.jpg",
@@ -32,13 +31,34 @@ export class AppComponent implements OnInit {
       "https://www.iamag.co/wp-content/uploads/2020/09/The-Art-of-Legends-of-Runeterra-205.jpg",
       "https://www.iamag.co/wp-content/uploads/2022/04/Legends-of-Runeterra-34.jpg",
       "https://www.iamag.co/wp-content/uploads/2022/04/Legends-of-Runeterra-7.jpg",
-      "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/76/76003.jpg",
-      "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/17/17014.jpg",
+      "https://sjc1.vultrobjects.com/cucdn/gallery-38/art/lor-swole-scout.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-hungry-owlcat.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-dark-star-zed.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-pool-party-fiora.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-pool-party-taric.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-zoe-level-2.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-cosmic-yasuo-level-2.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-twisted-fate-level-2.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-sun-guardian.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-swain-level-2.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-swain-level-2.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-the-grand-plaza.jpg",
+      "https://masteringruneterra.com/wp-content/uploads/cards/full/04IO004-full.webp",
+      "https://i.redd.it/g5h82dx73ta71.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-shyvana-dragon.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-soraka-level-2.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-stony-supressor.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-starspring.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-azir.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-kindred.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-helper-spirit.jpg",
+      "https://www.creativeuncut.com/gallery-38/art/lor-dragon-chow.jpg"
 
-      "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/20/20004.jpg",
-      "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/20/20026.jpg",
-      "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/236/236001.jpg",
-      "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-splashes/122/122015.jpg",
+
+
+
+
+
     ];
     var random = backgrounds[Math.floor(Math.random() * backgrounds.length)];
     document.body.style.backgroundImage = `url('${random}')`;
